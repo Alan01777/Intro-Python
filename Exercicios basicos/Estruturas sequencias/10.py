97617739147395bd750d3c7e9573768658f1c2cc
Alan01777 <camp.alan3@gmail.com>
2022-08-10 20:58:07 -0300

    Exercicios

---
+++ Exercicios basicos/Estruturas sequencias/10.py
@@ -0,0 +1,5 @@
+#Faça um Programa que peça a temperatura em graus Celsius,
+#transforme e mostre em graus Fahrenheit.
+
+celsius = float(input('Insira a temperatura em °C:'))
+fahrenheit = (celsius * 9 / 5) + 32
